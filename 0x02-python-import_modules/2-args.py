e4e45bc05433f6af88cb218913b843bd308fd361
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-09-09 15:29:47 +0000

    fix

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    l = len(sys.argv) - 1
-    print("{} argument{}{}"
-          .format(l, "" if l == 1 else "s", "." if l == 0 else ":"))
-    for i in range(l):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    from sys import argv
+    i = 1
+    if len(argv) < 2:
+        print("0 arguments.")
+    elif len(argv) < 3:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(len(argv) - 1))
+    for arg in argv[1:]:
+        print("{:d}: {:s}".format(i, arg))
+        i += 1
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for l in dir(hidden_4):
-        if l[0:2] != "__":
-            print(l)
+    for name in dir(hidden_4):
+        if name.startswith('__'):
+            continue
+        print("{:s}".format(name))
