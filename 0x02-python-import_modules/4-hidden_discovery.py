99b3f8bf722299228a6a3abeb6245bc420cc2fe1
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-09-13 17:00:21 +0000

    hidden fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    import hidden_4
-    for i in dir(hidden_4):
-        if not i.startswith("__"):
-            print("{:s}".format(i))
+import sys
+import hidden_4 as hidden
+
+if __name__ != "__main__":
+    exit()
+
+for name in dir(hidden):
+    if name[0:2] != "__":
+        print(name)
