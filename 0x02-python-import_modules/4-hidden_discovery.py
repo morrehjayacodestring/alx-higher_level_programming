a23d1d32bb174d6de4d7f8064eda0741b922d1e6
morrehjayacodestring <morrehjayammsgk@gmail.com>
2021-09-13 17:22:40 +0000

    hidden fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3.4
+#!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+    import hidden_4
+    all_dir = dir(hidden_4)
+    for i in range(0, len(all_dir)):
+        if "__" != all_dir[i][:2]:
+            print(all_dir[i])
