949e6a6627e0c72546fa58ee7032982727f5098a
morrehjayacodestring <morrehjayammsgk@gmail.com>
2022-01-08 06:14:24 -0800

    fix

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""
-fetches https://intranet.hbtn.io/status
-"""
+"""urllib usage"""
+import urllib.request
+import urllib.parse
+
+
 if __name__ == "__main__":
-        import urllib.request as request
-            with request.urlopen('https://intranet.hbtn.io/status') as r:
-                        html = r.read()
-                                print('Body response:')
-                                        print("\t- type: {}".format(type(html)))
-                                                print("\t- content: {}".format(html))
-                                                        print("\t- utf8 content: {}".format(html.decode('utf-8')))
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print('\t- type:', type(html))
+        print('\t- content:', html)
+        print('\t- utf8 content:', html.decode('UTF-8'))
