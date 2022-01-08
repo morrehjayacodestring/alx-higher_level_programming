52641df465e616f650c3e0c4067e96e3d546bf63
morrehjayacodestring <morrehjayammsgk@gmail.com>
2022-01-08 06:21:16 -0800

    f

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response"""
-if __name__ == "__main__":
-        import urllib.request
-            import urllib.parse
-                import sys
+"""
+sends a POST request
+"""
 
-                    value = {'email': sys.argv[2]}
-                        data = urllib.parse.urlencode(value)
-                            data = data.encode('ascii')
-                                req = urllib.request.Request(sys.argv[1], data)
-                                    with urllib.request.urlopen(req) as response:
-                                                print(response.read().decode("utf-8", "replace"))
+if __name__ == '__main__':
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+    data = urllib.parse.urlencode(value)
+    data = data.encode("ascii")
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        info = response.read()
+        print(info.decode("ascii"))
